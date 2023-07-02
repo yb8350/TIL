85753d9ccd76427fac57acdbc6e17e29e367c22f
yb8350 <yb8350@naver.com>
2023-07-03 03:59:39 +0900

    230702 알고리즘 1문제

---
+++ CodingTest/BOJ/Java/Main_B5_2393_Rook.java
@@ -0,0 +1,19 @@
+package com.pttdv.st;
+
+public class Main_B5_2393_Rook {
+	public static void main(String[] args) {
+		System.out.println("  ___  ___  ___\n" + 
+				"  | |__| |__| |\n" + 
+				"  |           |\n" + 
+				"   \\_________/\n" + 
+				"    \\_______/\n" + 
+				"     |     |\n" + 
+				"     |     |\n" + 
+				"     |     |\n" + 
+				"     |     |\n" + 
+				"     |_____|\n" + 
+				"  __/       \\__\n" + 
+				" /             \\\n" + 
+				"/_______________\\");
+	}
+}
