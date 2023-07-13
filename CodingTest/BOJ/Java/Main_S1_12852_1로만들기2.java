d3676d697041825c0b0652f8dedaa1cb74e54dd1
yb8350 <yb8350@naver.com>
2023-07-14 05:42:53 +0900

    230713 알고리즘 2문제

--- CodingTest/BOJ/Java/Main_S1_12852_1로만들기2.java
+++ CodingTest/BOJ/Java/Main_S1_12852_1로만들기2.java
@@ -1,12 +1,16 @@
 package com.pttdv.st0717;
 import java.util.*;
 import java.io.*;
-public class Main_S3_1463_1로만들기 { //15448kb	84ms
+public class Main_S1_12852_1로만들기2 { //15508kb	80ms => Java 5등
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		// 입력 : 숫자 N
 		int N = Integer.parseInt(in.readLine());
+		
+		// 풀이 : 다음 숫자의 인덱스 위치에 현재 숫자를 저장하여, 1을 만든 후 역순으로 따라가 출력
 		int[] arr = new int[N+1];
 		Queue<Integer> q = new LinkedList<>();
+		arr[N] = -1;
 		q.offer(N);
 		
 		while(!q.isEmpty()) {
@@ -14,13 +18,17 @@ public class Main_S3_1463_1로만들기 { //15448kb	84ms
 			
 			// 1을 만들었다면, 역순으로 숫자를 따라가며 sb에 저장
 			if(num == 1) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(1);
 				int cnt = 0;
 				int now = 1;
 				while(now != N) {
 					now = arr[now];
+					sb.insert(0, now + " ");
 					cnt++;
 				}
 				System.out.println(cnt);
+				System.out.println(sb);
 				break;
 			}
 			
